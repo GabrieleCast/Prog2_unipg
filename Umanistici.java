682400c3bd491606378c695145e0b686d663245b
GabrieleCast <lavanderia.fanello@gmail.com>
2016-07-12 18:27:35 +0200

    Create Umanistici.java

---
+++ Umanistici.java
@@ -0,0 +1,22 @@
+package progii_polimorfismo;
+
+/**
+ *
+ * @author GC
+ */
+public class Umanistici extends Universitari
+{
+    int biblioteche;
+    public Umanistici(int biblioteche, String nome, int cfu) {
+        super(nome, cfu);
+        this.biblioteche = biblioteche;
+    }
+    @Override
+    public int tassaDiIscrizione() 
+    {
+        int tassa;
+        tassa=(50+cfu)+(10*biblioteche);
+        return tassa;
+    }
+    
+}
